4876e9d77d8befe5e0c160e57e247aa343523fd8
Axel Ramos2121 <axel7.atrf@gmail.com>
2026-02-15 05:31:25 -0300

    0.1.0

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const express = require("express");
+const { checkTwitch } = require("./services/twitch"); // Importante
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -21,6 +22,14 @@ app.listen(PORT, () => {
 // ===== BOT =====
 client.once("ready", () => {
   console.log(`Bot listo como ${client.user.tag}`);
+
+  // Ejecuta una vez al iniciar
+  checkTwitch(client);
+
+  // Ejecuta cada 5 minutos (300000 ms)
+  setInterval(() => {
+    checkTwitch(client);
+  }, 300000);
 });
 
 client.login(process.env.DISCORD_TOKEN);
